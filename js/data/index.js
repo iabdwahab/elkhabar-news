a0921fc756e2b70517e841cc970a8c6ec3739203
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-04-13 17:04:12 +0200

    Linking Sports and Politics Categories with API

--- js/data/index.js
+++ js/data/index.js
@@ -4,100 +4,87 @@ const mainContentContainer = document.querySelector('.main-content .container');
 const contentSectionsContainer = document.querySelector('.content__sections');
 const websiteSections = ['economy', 'latest', 'main', 'politics', 'sports', 'technology'];
 
-  const v = `
-  <section class="content__section">
-    <div class="content__heading">
-      <h2 class="content__title" data-translate-name="technology">Technology</h2>
-      <a aria-label="label" href="#" class="content-heading__link">
-        <p data-translate-name="view_more">View More </p>
-        <i class="fa-solid fa-arrow-right"></i>
-      </a>
-    </div>
-  </section>
-  `
+
+var myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
   
-  
-  
-  var myHeaders = new Headers();
-  myHeaders.append("Accept", "application/json");
-  
-  var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-  
-  fetch("https://blog.ammarelgendy.online/api/home", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result)
+fetch("https://blog.ammarelgendy.online/api/home", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result)
 
 
-      // #################
-      // Start MainContent
-      // #################
-      const resultMain = result.data.main;
-      let mainContentHTML = ``;
-  
-      resultMain.forEach((mainNews, index) => {
-        mainContentHTML += `
-          <a aria-label="label" href="#" class="hero-news hero-news-${index}">
-            <img src="${mainNews.image_url}" alt="image" class="hero-news__image">
-            <p class="hero-news__type">${mainNews.category.name}</p>
-            <p class="hero-news__date">${formatDate(mainNews.date)}</p>
-            <p class="hero-news__author">${mainNews.publisher}</p>
-            <h3 class="hero-news__title">${mainNews.title[websiteLang]}</h3>
-            <p class="hero-news__description">
-              ${mainNews.content.en}
-            </p>
+    // #################
+    // Start MainContent
+    // #################
+    const resultMain = result.data.main;
+    let mainContentHTML = ``;
+
+    resultMain.forEach((mainNews, index) => {
+      mainContentHTML += `
+        <a aria-label="label" href="#" class="hero-news hero-news-${index}">
+          <img src="${mainNews.image_url}" alt="image" class="hero-news__image">
+          <p class="hero-news__type">${mainNews.category.name}</p>
+          <p class="hero-news__date">${formatDate(mainNews.date)}</p>
+          <p class="hero-news__author">${mainNews.publisher}</p>
+          <h3 class="hero-news__title">${mainNews.title[websiteLang]}</h3>
+          <p class="hero-news__description">
+            ${mainNews.content.en}
+          </p>
+        </a>
+      `;
+
+      mainContentContainer.innerHTML = mainContentHTML
+    });
+    // ###############
+    // End MainContent
+    // ###############
+
+
+
+    // ##############
+    // Start Sections
+    // ##############
+    const resultContent = result.data.content; // Type: Object;
+    
+    for (const section in resultContent) {
+      let conentCardsHTML = '';
+
+      resultContent[section].forEach(sectionNews => {
+        conentCardsHTML += `
+        <div class="content__card">
+          <a aria-label="label" href="#">
+            <img src="${sectionNews.image_url}" alt="image" class="card__image">
           </a>
-        `;
-
-        mainContentContainer.innerHTML = mainContentHTML
-      });
-      // ###############
-      // End MainContent
-      // ###############
-
-  
-
-      // ##############
-      // Start Sections
-      // ##############
-      const resultContent = result.data.content; // Type: Object;
-      
-      for (const section in resultContent) {
-        let conentCardsHTML = '';
-
-        resultContent[section].forEach(sectionNews => {
-          conentCardsHTML += `
-          <div class="content__card">
-            <a aria-label="label" href="#">
-              <img src="${sectionNews.image_url}" alt="image" class="card__image">
-            </a>
-            <a aria-label="label" href="#">
-              <h3 class="card__title">${sectionNews.title[websiteLang]}</h3>
-            </a>
-            <div class="card__info">
-              <p class="card__author">${sectionNews.publisher}</p>
-              <p class="card__date">${formatDate(sectionNews.date)}</p>
-            </div>
-            <p class="card__description">${sectionNews.content[websiteLang]}</p>
+          <a aria-label="label" href="#">
+            <h3 class="card__title">${sectionNews.title[websiteLang]}</h3>
+          </a>
+          <div class="card__info">
+            <p class="card__author">${sectionNews.publisher}</p>
+            <p class="card__date">${formatDate(sectionNews.date)}</p>
           </div>
-          `;
-        });
+          <p class="card__description">${sectionNews.content[websiteLang]}</p>
+        </div>
+        `;
+      });
 
 
-        document.querySelector(`.${section}__content-cards`).innerHTML = conentCardsHTML;
-      }
+      document.querySelector(`.${section}__content-cards`).innerHTML = conentCardsHTML;
+    }
 
 
 
-      // ##############
-      // End Sections
-      // ##############
+    // ##############
+    // End Sections
+    // ##############
 
 
-  
-    })
-    .catch(error => console.log('error', error));
+
+  })
+  .catch(error => console.log('error', error));
---
+++ js/data/politics-sports.js
@@ -0,0 +1,87 @@
+
+const pageCategory = document.querySelector('html').dataset.pageCategory;
+const websiteLang = localStorage.getItem('lang') || 'en';
+
+
+const latestCardsContainer = document.querySelector('.latest__cards');
+const normalNewsContainer = document.querySelector('.section-news__container');
+
+
+
+// Fetching Function
+var myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch(`https://blog.ammarelgendy.online/api/category/${pageCategory}?pageSize=8`, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+
+
+    const latestResult = result.data.latest;
+    let latestHTML = '';
+
+    for (let i = 0; i < 3; i++) {
+      const latestNews = latestResult[i];
+
+      latestHTML += `
+        <div class="latest__card">
+          <a aria-label="label" href="#" class="latest__image-link">
+            <img src="${latestNews.image_url}" alt="image" class="latest__image">
+          </a>
+          
+          <div class="latest-card__text">
+            <a aria-label="label" href="#">
+              <h3 class="latest__title card__title">${latestNews.title[websiteLang]}</h3>
+            </a>
+            <div class="latest__details">
+              <p class="latest__author">${latestNews.publisher}</p>
+              <p class="latest__date">${formatDate(latestNews.date)}</p>
+            </div>
+            <p class="latest__description">${latestNews.content[websiteLang]}</p>
+          </div>
+        </div>
+      `;
+    }
+
+    latestCardsContainer.innerHTML = latestHTML;
+
+
+
+    // #########
+    // Start content Cards
+    // #########
+    const normalResult = result.data.normal;
+    let normalHTML = '';
+
+    for (let i = 0; i < 8; i++) {
+      const normalNews = normalResult[i];
+
+      normalHTML += `
+      <div class="content__card">
+        <a aria-label="label" href="#">
+          <img src="${normalNews.image_url}" alt="image" class="card__image">
+        </a>
+        <a aria-label="label" href="#">
+          <h3 class="card__title">${normalNews.title[websiteLang]}</h3>
+        </a>
+        <div class="card__info">
+          <p class="card__author">${normalNews.publisher}</p>
+          <p class="card__date">${formatDate(normalNews.date)}</p>
+        </div>
+        <p class="card__description">${normalNews.content[websiteLang]}</p>
+      </div>
+      `;
+    }
+
+    normalNewsContainer.innerHTML = normalHTML;
+
+  })
+  .catch(error => console.log('error', error));
+
