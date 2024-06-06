5ed2f9d201ed3d6b8b23dae41b1ba0540e7b735a
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-06-06 17:49:29 +0300

    Change social links and Make it more reusable

---
+++ js/data/utils/globalHTML.js
@@ -0,0 +1,20 @@
+document.querySelector('.header__social-links').innerHTML = `
+  <li><a aria-label="label" href="https://www.facebook.com"><i class="fa-brands fa-facebook-f social-links__icon"></i></a></li>
+  <li><a aria-label="label" href="https://www.twitter.com"><i class="fa-brands fa-twitter social-links__icon"></i></a></li>
+  <li><a aria-label="label" href="https://www.instagram.com"><i class="fa-brands fa-square-instagram social-links__icon"></i></a></li>
+`;
+
+document.querySelector('.footer-social__list').innerHTML = `
+  <li>
+    <a aria-label="label" href="https://www.facebook.com" class="footer-social__link"><i class="fa-brands fa-facebook-f footer-social__icon"></i></a>
+  </li>
+  <li>
+    <a aria-label="label" href="https://www.twittercom" class="footer-social__link"><i class="fa-brands fa-twitter footer-social__icon"></i></a>
+  </li>
+  <li>
+    <a aria-label="label" href="https://www.instagram.com" class="footer-social__link"><i class="fa-brands fa-square-instagram footer-social__icon"></i></a>
+  </li>
+  <li>
+    <a aria-label="label" href="https://www.linkedin.com" class="footer-social__link"><i class="fa-brands fa-linkedin footer-social__icon"></i></a>
+  </li>
+`
